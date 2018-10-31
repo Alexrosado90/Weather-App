f3e40b06ec40d9dd866e35047db1f103565f861c
Alex Rosado <Ace@Macbooks-MacBook-Pro-4.local>
2018-10-31 18:00:02 -0400

    weather model created

---
+++ server/models/weather.js
@@ -0,0 +1,19 @@
+const request = requre('request-promise');
+
+const API_KEY = '24df04da3977b33c8bbd2dde96bf785e'
+
+class weather {
+    static retrieveByCity (city, callback) {
+        request({
+            uri:`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
+            json: true
+        }).then(function (res) {
+            callback(res);
+        }).catch(function (err) {
+            console.log(err);
+            callback({ error: 'Could not reach OpenWeatherMap API.'})
+        });
+    }
+}
+
+module.exports = weather;
